4776570edbf035ddfefda25ff891d5ce1410a47c
Gabriel Sailer <gabriel.sailer@riseup.net>
2018-01-21 21:26:02 +0100

    get_url_token

--- sptygenre/auth/auth.py
+++ sptygenre/auth/auth.py
@@ -1,5 +1,6 @@
 import spotipy
 import spotipy.util as util
+from spotipy import oauth2
 from sptygenre import exceptions
 
 def authorize(username):
@@ -9,3 +10,16 @@ def authorize(username):
 		return spotipy.Spotify(auth=token)
 	else:
 		raise exceptions.TokenException(username)
+
+# returns spotipy OAuth Object
+def get_url_token(username):
+	_client_id = ''
+	_client_secret = ''
+	_redirect_uri = ''
+	scope = 'playlist-read-private'
+	cache_path = ".cache-" + username
+    sp_oauth = oauth2.SpotifyOAuth(_client_id, _client_secret, _redirect_uri, 
+        scope=scope, cache_path=cache_path)
+    token_info = sp_oauth.get_cached_token()
+    return sp_oauth
+
