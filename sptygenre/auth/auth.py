57d0f66a8593440d01ebab3ee5a6f07a6a788269
Gabriel Sailer <gabriel.sailer@riseup.net>
2018-01-22 18:56:56 +0100

    fix indentation

--- sptygenre/auth/auth.py
+++ sptygenre/auth/auth.py
@@ -4,25 +4,25 @@ from spotipy import oauth2
 from sptygenre import exceptions
 
 def authorize(username):
-	scope = 'playlist-read-private'
-	token = util.prompt_for_user_token(username, scope)
-	if token:
-		return spotipy.Spotify(auth=token)
-	else:
-		raise exceptions.TokenException(username)
+    scope = 'playlist-read-private'
+    token = util.prompt_for_user_token(username, scope)
+    if token:
+        return spotipy.Spotify(auth=token)
+    else:
+        raise exceptions.TokenException(username)
 
 # returns auth url
 def get_auth_url(username):
-	_client_id = ''
-	_client_secret = ''
-	_redirect_uri = ''
-	scope = 'playlist-read-private'
-	cache_path = ".cache-" + username
+    _client_id = ''
+    _client_secret = ''
+    _redirect_uri = ''
+    scope = 'playlist-read-private'
+    cache_path = ".cache-" + username
     sp_oauth = oauth2.SpotifyOAuth(_client_id, _client_secret, _redirect_uri, scope=scope, cache_path=cache_path)
     return sp_oauth.get_authorize_url()
 
 # returns token for response url
 def get_token(sp_oauth, response_url):
-	code = oauth2.parse_response_code(response_url)
-	token = sp_oauth.get_access_token(code)
-	return token['access_token']
+    code = oauth2.parse_response_code(response_url)
+    token = sp_oauth.get_access_token(code)
+    return token['access_token']
